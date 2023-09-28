9c92ee06c335896b76ae3b3ebf832567d8c591a7
Michael Grazebrook <michael@grazebrook.com>
2023-09-28 20:51:33 +0100

    naff animation

--- dojo_pet.py
+++ dojo_pet.py
@@ -1,5 +1,5 @@
 import pygame
-
+import math
 
 class PetModel():
     """
@@ -7,14 +7,30 @@ class PetModel():
     def __init__(self):
         self.model = [
             # Array of pairs of points representing our pet
-            ((100, 100), (200, 400)),
-            ((100, 100), (50, 50)),
-            ((100, 100), (50, 150))
+            ((200, 700), (500, 700)),
+            ((500, 700), (600, 500)),
+            ((600, 500), (700, 640)),
+            ((600, 500), (650, 680)),
         ]
+
+    def update(self, seconds):
+        # Mouth length: 120
+        # Initial angle: 30 & 45 degrees down
+        # Top jaw stationary
+        # "head" is the point where the jaw lines meet
+        # 2 seconds?
+        seconds %= 2000
+        if seconds < 1000:
+            t = seconds
+        else:
+            t = seconds - 1000
+        dx = 120 * math.sin(seconds * 2 * math.pi / 1000)
+        dy = 120 * math.cos(seconds * 2 * math.pi / 1000)
+        self.model[-1] = ((600, 500), (600 + dx, 500 + dy))
 
     def draw(self, surface):
         for p1, p2 in self.model:
-            pygame.draw.line(surface, "green", p1, p2)
+            pygame.draw.line(surface, "green", p1, p2, width=4)
 
 class DojoPet():
     def __init__(self):
@@ -23,7 +39,10 @@ class DojoPet():
         pygame.display.set_caption('Dojo Pet')
         self.screen = pygame.display.set_mode((1200, 960))
 
+        self.fps = 60
         self.clock = pygame.time.Clock()
+        self.start = self.clock.get_time()
+
         self.reset()
 
 
@@ -42,7 +61,11 @@ class DojoPet():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+
+                time = self.clock.get_time() - self.start
+                self.model.update(time)
                 self.draw()
+                self.clock.tick(self.fps)
 
 
 
